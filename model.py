27bc750b71a57c79cfd49af523296fc5c15f0325
Nathan-Yap <65648604+Nathan-Yap@users.noreply.github.com>
2024-10-06 14:55:17 -0400

    Update model.py

--- model.py
+++ model.py
@@ -33,17 +33,6 @@ class LSTMClassifier(nn.Module):
 
 	def forward(self, batch, lengths):
 		
-		self.hidden = self.init_hidden(batch.size(-1))
-
-		embeds = self.embedding(batch)
-		packed_input = pack_padded_sequence(embeds, lengths)
-		outputs, (ht, ct) = self.lstm(packed_input, self.hidden)
-
-		# ht is the last hidden state of the sequences
-		# ht = (1 x batch_size x hidden_dim)
-		# ht[-1] = (batch_size x hidden_dim)
-		output = self.dropout_layer(ht[-1])
-		output = self.hidden2out(output)
-		output = self.softmax(output)
+		# TODO: Fill in here
 
 		return output
